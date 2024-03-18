782ddb2e5c504e49c61a50efa65c6ba071c55791
Filip Polatynski <fpolatynski@gmail.com>
2024-03-18 20:04:19 +0100

    adding __str__ to models

--- project4/network/models.py
+++ project4/network/models.py
@@ -5,10 +5,17 @@ from django.db import models
 class User(AbstractUser):
     follows = models.ManyToManyField("self", symmetrical=False, related_name="followers")
 
+    def __str__(self):
+        return self.username
 
-class Posts(models.Model):
+
+class Post(models.Model):
     text = models.TextField(max_length=512)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
     timestamp = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name="liked_posts")
     unlikes = models.ManyToManyField(User, related_name="unliked_posts")
+
+    def __str__(self):
+        return f"{self.owner} post at {self.timestamp}"
+
