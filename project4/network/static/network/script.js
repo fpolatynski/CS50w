3906caa574cc34070ee46dc8d94d8b4e93799291
Filip Polatynski <fpolatynski@gmail.com>
2024-03-19 12:56:17 +0100

    Post visual side

--- project4/network/static/network/script.js
+++ project4/network/static/network/script.js
@@ -20,18 +20,33 @@ let amount = 5;
 
 function display(data){
     data["posts"].forEach(post => {
-        const post_div = document.createElement('div')
-        post_div.className = 'post'
-        const owner_div = document.createElement('div')
-        owner_div.className = 'owner'
-        const text_div = document.createElement('div')
-        text_div.className = 'text'
+        const post_div = document.createElement('div');
+        post_div.className = 'post';
+        const owner_div = document.createElement('div');
+        owner_div.className = 'owner';
+        const text_div = document.createElement('div');
+        text_div.className = 'text';
+        const like_div = document.createElement('div');
+        like_div.className = 'like';
+        const com_div = document.createElement('div');
+        com_div.className = 'com';
+        const date_div = document.createElement('div');
+        date_div.className = 'date';
+        const edit_div = document.createElement('div');
+        edit_div.className = 'edit';
 
-        text_div.innerHTML = post.text
+        text_div.innerHTML = post.text;
         owner_div.innerHTML = post.owner;
-        post_div.append(owner_div)
-        post_div.append(text_div)
-        document.querySelector("#all_posts").append(post_div)
+        like_div.innerHTML = "<i class=\"bi-hand-thumbs-up\" style=\"font-size: 1.8rem;\"></i>";
+        com_div.innerHTML = "<a>comments</a>";
+        date_div.innerHTML = post.timestamp.toString().split('T')[0];
+
+        post_div.append(owner_div);
+        post_div.append(text_div);
+        post_div.append(date_div);
+        post_div.append(like_div);
+        post_div.append(com_div);
+        document.querySelector("#all_posts").append(post_div);
     })
     start += amount;
 }
